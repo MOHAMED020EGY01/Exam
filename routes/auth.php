6f3f5bc3f8ca5ee955b1f9305b04a678cee614b1
Mohamed Hussein <mohamed02051010@gmail.com>
2026-04-02 22:41:26 +0200

    test

--- routes/auth.php
+++ routes/auth.php
@@ -1,11 +1,11 @@
-<?php 
+<?php
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\SocialiteController;
 
+Route::get('/login', [SocialiteController::class, 'login'])->name('login');
 Route::group([
     'middleware' => 'guest',
 ], function () {
-    Route::get('/login', [SocialiteController::class, 'login'])->name('login');
     Route::get('auth/{provider}', [SocialiteController::class, 'redirect'])
         ->name('auth.redirect');
     Route::get('auth/{provider}/callback', [SocialiteController::class, 'callback'])
